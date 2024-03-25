47dfefc491460f703230688fbad8d3476a4acfdf
Pronoy568 <pronoydas463@gamil.com>
2024-03-25 19:57:05 +0600

    Create Product Route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -24,7 +24,78 @@ async function run() {
     console.log("Connected to MongoDB");
 
     const db = client.db("vegStore");
-    // const collection = db.collection('');
+    const vegCollection = db.collection("vegetable");
+
+    //create veg
+    app.post("/api/v1/products", async (req, res) => {
+      const {
+        image,
+        title,
+        rating,
+        price,
+        brand,
+        description,
+        sale,
+        salePrice,
+      } = req.body;
+      const result = await vegCollection.insertOne({
+        image,
+        title,
+        rating,
+        price,
+        brand,
+        description,
+        sale,
+        salePrice,
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "Products created successfully",
+        data: result,
+      });
+    });
+
+    //get all veg
+    app.get("/api/v1/products", async (req, res) => {
+      const { category } = req.query;
+
+      const filter = {};
+      if (category) {
+        filter.category = category;
+      }
+
+      try {
+        const result = await vegCollection.find(filter).toArray();
+
+        res.status(200).json({
+          success: true,
+          message: "Products retrieved successfully",
+          data: result,
+        });
+      } catch (error) {
+        res.status(500).json({
+          success: false,
+          message: "Failed to retrieve products",
+          error: error.message,
+        });
+      }
+    });
+
+    //get single veg
+    app.get("/api/v1/products/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await vegCollection.findOne(query);
+
+      res.status(200).json({
+        success: true,
+        message: "Products retrieved successfully",
+        data: result,
+      });
+    });
 
     // route
     app.get("/", (req, res) => {
