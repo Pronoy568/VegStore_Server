7aefd593dfc7c6bc725a0ee2a7c3a0026e1dcc30
Pronoy568 <pronoydas463@gamil.com>
2024-03-25 18:44:52 +0600

    Initial Setup

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection URL
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("vegStore");
+    // const collection = db.collection('');
+
+    // route
+    app.get("/", (req, res) => {
+      const serverStatus = {
+        message: "Server is running smoothly",
+        timestamp: new Date(),
+      };
+      res.json(serverStatus);
+    });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
